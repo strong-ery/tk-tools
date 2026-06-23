a174088e182d2ff983353a72808ea9d9fa0b2c8f
strong-ery <dstrongbusiness@gmail.com>
2026-06-23 16:40:39 -0600

    entry point

--- src/main.py
+++ src/main.py
@@ -1,11 +1,20 @@
 from classes.util.ZstdUtils import ZstdUtils
 from classes.util.ArchiveUtils import ArchiveUtils
+from config import Config
+import json
+from pathlib import Path
+from appglobals import appglobals
 
 def main():
-    ZstdUtils(r"D:\ZeldaModding\romfs 121 dump\Pack\ZsDic.pack.zs")
+    config = Config(appglobals.APPDATA_PATH / "config.json")
+    if config.was_just_created:
+        print("Default config created. Please edit config.json with the correct paths and restart the program.")
+        return
+
+    ZstdUtils(Path(config.get("romfs_path")) / "Pack" / "ZsDic.pack.zs")
 
     try:
-        result = ArchiveUtils.GetFileFromArchive(r"D:\ZeldaModding\romfs 121 dump\Pack\ZsDic.pack.zs", "zs.zsdic")
+        result = ArchiveUtils.GetArchiveContentsAsList(Path(config.get("romfs_path")) / "Pack" / "Actor" / "Enemy_Bokoblin_Junior.pack.zs")
         print(result)
     except FileNotFoundError as e:
         print(f"Error: {e}")
